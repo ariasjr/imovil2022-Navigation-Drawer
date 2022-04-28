dca0561cb9b1eb0d58c3ef7bbae9841b7cd26b4c
UO271489 <UO271489@uniovi.es>
2022-04-28 17:06:16 +0200

    Se implementan circuitos y una implementación de selección de circuitos de prueba

--- app/src/main/java/es/imovil/fcrtrainer/ui/digital/ejercicioCircuitosDigitales/FragmentExerciceCircuitosDigitales.kt
+++ app/src/main/java/es/imovil/fcrtrainer/ui/digital/ejercicioCircuitosDigitales/FragmentExerciceCircuitosDigitales.kt
@@ -1,16 +1,18 @@
 package es.imovil.fcrtrainer.ui.digital.ejercicioCircuitosDigitales
 
+import android.content.res.Resources
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-
+import es.imovil.fcrtrainer.R
 import es.imovil.fcrtrainer.databinding.FragmentExerciceCircuitosDigitalesBinding
-
-
+import java.util.*
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 
 class FragmentExerciceCircuitosDigitales : Fragment() {
@@ -19,7 +21,10 @@ class FragmentExerciceCircuitosDigitales : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private var a:Boolean=false
+    private var b:Boolean=false
+    private var c:Boolean=false
+    val r = Random(73426472834)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +36,27 @@ class FragmentExerciceCircuitosDigitales : Fragment() {
 
         _binding = FragmentExerciceCircuitosDigitalesBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        generarValores()
+
+
+        when(generaCircuitos()){
+            0->{
+                var resources: Resources = requireContext().resources
+                var resourceId = resources.getIdentifier("circuito1","drawable",requireContext().packageName)
+                binding.circuitos.setImageDrawable(resources.getDrawable(R.drawable.circuito1))
+            }
+            1->{
+                var resources: Resources = requireContext().resources
+                var resourceId = resources.getIdentifier("circuito2","drawable",requireContext().packageName)
+                binding.circuitos.setImageDrawable(resources.getDrawable(R.drawable.circuito2))
+            }
+            else->{
+                var resources: Resources = requireContext().resources
+                var resourceId = resources.getIdentifier("circuito3","drawable",requireContext().packageName)
+                binding.circuitos.setImageDrawable(resources.getDrawable(R.drawable.circuito3))
+            }
+        }
+
 
         val textView: TextView = binding.textSlideshow
         slideshowViewModel.text.observe(viewLifecycleOwner) {
@@ -42,5 +68,27 @@ class FragmentExerciceCircuitosDigitales : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    fun generarValores(){
+        a=r.nextBoolean()
+        b=r.nextBoolean()
+        c=r.nextBoolean()
+    }
+    fun generaCircuitos():Int {
+        when (Random(443).nextInt(0..2)) {
+            0 -> return 0
+            1 -> return 1
+            else -> return 2
+        }
+    }
+
+    fun circuito1():Boolean{
+        return (a != b) || !(b && c)
+    }
+    fun circuito2():Boolean{
+        return (a || b) != (b && c)
+    }
+    fun circuito3():Boolean{
+        return (a != b) && !(b || c)
     }
 }
