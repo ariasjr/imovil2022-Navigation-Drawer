55aec8463df001a402630ed41deb8342a037a164
Adolfo <uo272695@uniovi.es>
2022-05-01 16:32:08 +0200

    Avance coma flotante, creacion clases principales y elementos necesarios en las carpetas de values

---
+++ app/src/main/java/es/imovil/fcrtrainer/ui/codes/comaFlotante/ComaFlotante.kt
@@ -0,0 +1,22 @@
+package es.imovil.fcrtrainer.ui.codes.comaFlotante
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import es.imovil.fcrtrainer.R
+import kotlin.math.pow
+import kotlin.random.Random
+import java.util.Timer
+import kotlin.concurrent.schedule
+
+class ComaFlotante : Fragment {
+
+}
