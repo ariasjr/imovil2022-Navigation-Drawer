349a2a3914878c06d7c793dc88d0c3744cd9e4ed
UO271489 <79192727+UO271489@users.noreply.github.com>
2022-05-04 00:38:46 +0200

    Cambios menores como añadir strings a strings.xml

--- app/src/main/java/es/imovil/fcrtrainer/ui/digital/ejercicioCircuitosDigitales/ExerciceCircuitosDigitalesViewModel.kt
+++ app/src/main/java/es/imovil/fcrtrainer/ui/digital/ejercicioCircuitosDigitales/ExerciceCircuitosDigitalesViewModel.kt
@@ -1,13 +1,15 @@
 package es.imovil.fcrtrainer.ui.digital.ejercicioCircuitosDigitales
 
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import es.imovil.fcrtrainer.R
 
 class ExerciceCircuitosDigitalesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is exercice of Digital Circuits Fragment"
+        value = "Introduce la salida S del circuito digital"
     }
     val text: LiveData<String> = _text
 }
